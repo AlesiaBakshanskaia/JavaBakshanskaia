175ea778bb52937a671ec846544e9f9d14d88ea8
Alesia <andomyel@yandex.ru>
2023-03-08 17:38:20 +0300

    Исправлена 1 задача 2 ДЗ

--- src/main/java/homeWork2/Task1.java
+++ src/main/java/homeWork2/Task1.java
@@ -3,13 +3,11 @@ package homeWork2;
 //1. Напишите метод, который принимает на вход строку (String) и определяет является ли строка палиндромом (возвращает boolean значение).
 public class Task1 {
     public static void main(String[] args) {
-        String str = "";
-        if (str.length()==0) {
+        String str = "1accan1nacca1";
+        if (str.length() == 0) {
             System.out.println("Строка пуста");
-            return;
-        } else if (str.length()==1) {
+        } else if (str.length() == 1) {
             System.out.println("Строка содержит только 1 символ");
-            return;
         }
 
         boolean resultCheckStringForPalindrome = checkStringForPalindrome(str);
@@ -22,9 +20,7 @@ public class Task1 {
 
     private static boolean checkStringForPalindrome(String str) {
         for (int i = 0; i < str.length() / 2; i++) {
-            if (str.charAt(i) == str.charAt(str.length() - 1 - i)) {
-                continue;
-            } else {
+            if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
                 return false;
             }
         }
